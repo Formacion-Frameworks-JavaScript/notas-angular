f138d1c6aeea1800053f05385cbfcfdb9d5c0b7c
Mario <mario@mariogl.com>
2020-11-18 09:54:08 +0100

    Borrar nota

--- src/app/nota/nota.component.ts
+++ src/app/nota/nota.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Nota } from '../datos/nota-interface';
+import { NotasService } from '../notas.service';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -12,7 +13,12 @@ export class NotaComponent implements OnInit {
   @Input()
   public alumno: Nota;
 
-  constructor() { }
+  constructor(private notasService: NotasService) { }
+
+  public borrarNota(e: Event): void {
+    e.preventDefault();
+    this.notasService.borrarNota(this.alumno);
+  }
 
   ngOnInit(): void {
   }
