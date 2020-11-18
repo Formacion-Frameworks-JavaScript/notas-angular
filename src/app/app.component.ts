4a2ef53c15c401177f7fc6f3b699e2fd5aa0c536
Mario <mario@mariogl.com>
2020-11-18 07:18:09 +0100

    Pasándole la letra al componente Aula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public cargando = false;
   public mostrarFormulario = '';
+  public letrasAulas = ['A', 'B', 'C'];
 }
--- src/app/aula/aula.component.ts
+++ src/app/aula/aula.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'not-aula',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aula.component.css']
 })
 export class AulaComponent implements OnInit {
+
+  @Input()
+  public letra = '';
 
   constructor() { }
 
